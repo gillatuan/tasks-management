f8fff41c8d6bdb62fbae50e3b503c2a77a6d24a1
TuanBui <tuan.buidoanngoc@gmail.com>
2024-10-27 21:31:58 +0700

    add student and lesson using graphql and mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,28 @@
-import { Module } from '@nestjs/common';
+import { LessonModule } from '@/lesson/lesson.module';
 import { TasksModule } from '@/tasks/tasks.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from '@/lesson/lesson.entity';
+import { StudentModule } from '@/student/student.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    LessonModule,
+    StudentModule,
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson]
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
 })
 export class AppModule {}
